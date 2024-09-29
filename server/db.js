0805d7f80ec9c0e9447c257637e25406f88fb3a7
Joel Kasisi <joelkasisi10@gmail.com>
2024-09-29 04:24:54 +0200

    fix

---
+++ server/db.js
@@ -0,0 +1,10 @@
+"use strict";
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    productImage: String,
+    productName: String,
+    productDetails: [String],
+    modelsSizes: [String]
+});
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
